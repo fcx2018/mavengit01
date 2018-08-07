a270578565b7f054ef69a5e758a518802f97faab
fcx2018 <42162655+fcx2018@users.noreply.github.com>
2018-08-07 15:56:48 +0800

    Update pet.java
    描述不可以

--- src/main/java/com/pojo/pet.java
+++ src/main/java/com/pojo/pet.java
@@ -3,6 +3,6 @@ package com.pojo;
 public class pet {
     public void show(){
         System.out.println("i am e pet");
-        System.out.println("12312");
+        System.out.println("12312");//新增代码不正确
     }
 }
